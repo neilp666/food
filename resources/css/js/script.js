0dcb2b8521f537be9637401350aa4360836829e7
neil patel <neil@myindianweddingapp.com>
2019-03-11 11:09:49 +0000

    Added smooth scrolling to 2 CTA buttons

--- resources/css/js/script.js
+++ resources/css/js/script.js
@@ -1,3 +1,5 @@
+// for the sticky navigation
+
 $(document).ready(function() {
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
@@ -7,5 +9,10 @@ $(document).ready(function() {
         }
     }, {
         offset: '60px;'
-    })
+    });
+
+    // scroll on buttons 
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
 });
