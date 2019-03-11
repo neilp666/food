07342445674fb094fa05f1a3d6ca25002f648cd4
neil patel <neil@myindianweddingapp.com>
2019-03-11 13:37:09 +0000

    Added animate.css to have fadein animations of phone & images

--- resources/css/js/script.js
+++ resources/css/js/script.js
@@ -53,5 +53,23 @@ $('a[href*="#"]')
   }
 });
 
+// animate.css options 
+  $('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeIn');
+  }, { 
+    offset: '50%'
+  });
+
+  $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animated fadeInUp');
+  }, { 
+    offset: '50%'
+  });
+
+  $('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').addClass('animated fadeIn');
+  }, { 
+    offset: '50%'
+  });
 
 });
